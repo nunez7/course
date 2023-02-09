abac10f0a67e87b5f60dc308ca5261dcb4b4312e
Felix Javier Nuñez Gonzalez <33491472+nunez7@users.noreply.github.com>
2023-02-09 16:50:30 -0700

    Agregando servicios de alumno

--- src/app/services/alumno.service.ts
+++ src/app/services/alumno.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Alumno } from '../models/alumno';
 
 @Injectable({
   providedIn: 'root'
@@ -7,5 +9,34 @@ import { HttpClient } from '@angular/common/http';
 export class AlumnoService {
   private baseEndpoint = 'http://localhost:8090/api/alumnos';
 
+  private cabeceras: HttpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
+
   constructor(private http: HttpClient) { }
+
+  public listar(): Observable<Alumno[]> {
+    return this.http.get<Alumno[]>(this.baseEndpoint);
+  }
+
+  public listarPaginas(page: string, size: string): Observable<any> {
+    const params = new HttpParams()
+    .set('page', page)
+    .set('size', size);
+    return this.http.get<any> (`${this.baseEndpoint}/pagina`, {params: params});
+  }
+
+  public ver(id:number): Observable<Alumno>{
+    return this.http.get<Alumno> (`${this.baseEndpoint}/${id}`);
+  }
+
+  public crear(alumno: Alumno): Observable<Alumno>{
+    return this.http.post<Alumno> (this.baseEndpoint, alumno, {headers: this.cabeceras});
+  }
+
+  public editar(alumno: Alumno): Observable<Alumno>{
+    return this.http.put<Alumno>  (`${this.baseEndpoint}/${alumno.id}`, alumno, {headers: this.cabeceras});
+  }
+
+  public eliminar(id:number): Observable<void>{
+    return this.http.delete<void>(`${this.baseEndpoint}/${id}`);
+  }
 }
