2d45e7eb45c92e700971379efe25aa92a7de876a
Felix Javier Nuñez Gonzalez <33491472+nunez7@users.noreply.github.com>
2023-02-09 16:51:03 -0700

    Vista de lista de alumnos

--- src/app/components/alumnos/alumnos.component.ts
+++ src/app/components/alumnos/alumnos.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Alumno } from 'src/app/models/alumno';
+import { AlumnoService } from 'src/app/services/alumno.service';
 
 @Component({
   selector: 'app-alumnos',
   templateUrl: './alumnos.component.html',
   styleUrls: ['./alumnos.component.css']
 })
-export class AlumnosComponent {
+export class AlumnosComponent implements OnInit{
 
   titulo = 'Listado de alumnos';
+  alumnos: Alumno[];
 
+  constructor(private service: AlumnoService){}
+
+  ngOnInit(): void {
+      this.service.listar().subscribe(alumnos =>this.alumnos = alumnos);
+  }
 }
