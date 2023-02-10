69a8d5446d257234a2c46a5e37105a9d7c775ebc
Felix Javier Nuñez Gonzalez <33491472+nunez7@users.noreply.github.com>
2023-02-10 16:29:15 -0700

    Agregando y modificando con foto

--- src/app/components/alumnos/alumnos-form.component.ts
+++ src/app/components/alumnos/alumnos-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Alumno } from 'src/app/models/alumno';
 import { AlumnoService } from 'src/app/services/alumno.service';
+import Swal from 'sweetalert2';
 import { CommonFormComponent } from '../commont-form.component';
 
 @Component({
@@ -27,5 +28,52 @@ extends CommonFormComponent<Alumno, AlumnoService> implements OnInit {
     seleccionarFoto(event: any):void{
       this.fotoSeleccionada = event.target.files[0];
       console.info(this.fotoSeleccionada);
+    }
+
+    public override crear(): void{
+      if(!this.fotoSeleccionada){
+        super.crear();
+      }else{
+        this.service.crearConFoto(this.model, this.fotoSeleccionada).subscribe({
+          next: (params) => {
+            //console.log('queryParams', params);
+            this.model = params;
+          },
+          complete: () => {
+            console.log('Creado');
+            Swal.fire('Nuevo: ', `Alumno ${this.model.nombre} creado con éxito`, 'success');
+            this.router.navigate([this.redirect]);
+          },
+          error: (e) => {
+            if (e.status === 400) {
+              this.error = e.error;
+              console.log(this.error);
+            }
+          }
+        });
+      }
+    }
+
+    public override editar(): void{
+      if(!this.fotoSeleccionada){
+        super.editar();
+      }else{
+        this.service.editarConFoto(this.model, this.fotoSeleccionada).subscribe({
+          next: (params) => {
+            //console.log('queryParams', params);
+            this.model = params;
+          },
+          complete: () => {
+            Swal.fire('Modificado: ', `Alumno ${this.model.nombre} modificado con éxito`, 'success');
+            this.router.navigate([this.redirect]);
+          },
+          error: (e) => {
+            if (e.status === 400) {
+              this.error = e.error;
+              console.log(this.error);
+            }
+          }
+        });
+      }
     }
 }
--- src/app/services/alumno.service.ts
+++ src/app/services/alumno.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
 import { Alumno } from '../models/alumno';
 import { CommonService } from './common.service';
 
@@ -14,4 +15,21 @@ export class AlumnoService extends CommonService<Alumno>{
   
   protected override baseEndpoint  = 'http://localhost:8090/api/alumnos';
 
+  public crearConFoto(alumno: Alumno, archivo:File):Observable<Alumno>{
+    const formData = new FormData();
+    formData.append('archivo', archivo);
+    formData.append('nombre', alumno.nombre);
+    formData.append('apellido', alumno.apellido);
+    formData.append('email', alumno.email);
+    return this.http.post<Alumno>(this.baseEndpoint+'/crear-con-foto', formData);
+  }
+
+  public editarConFoto(alumno: Alumno, archivo:File):Observable<Alumno>{
+    const formData = new FormData();
+    formData.append('archivo', archivo);
+    formData.append('nombre', alumno.nombre);
+    formData.append('apellido', alumno.apellido);
+    formData.append('email', alumno.email);
+    return this.http.put<Alumno>(`${this.baseEndpoint}/editar-con-foto/${alumno.id}`, formData);
+  }
 }
