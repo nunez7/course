e5e718d808fd61d543049df2f884b203681d5569
Felix Javier Nuñez Gonzalez <33491472+nunez7@users.noreply.github.com>
2023-02-09 19:14:01 -0700

    Creando componente form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlumnosFormComponent } from './components/alumnos/alumnos-form.component';
 import { AlumnosComponent } from './components/alumnos/alumnos.component';
 import { CursosComponent } from './components/cursos/cursos.component';
 import { ExamenesComponent } from './components/examenes/examenes.component';
@@ -7,6 +8,7 @@ import { ExamenesComponent } from './components/examenes/examenes.component';
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'cursos'},
   {path: 'alumnos', component: AlumnosComponent},
+  {path: 'alumnos/form', component: AlumnosFormComponent},
   {path: 'cursos', component: CursosComponent},
   {path: 'examenes', component: ExamenesComponent},
 ];
---
+++ src/app/components/alumnos/alumnos-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alumnos-form',
+  templateUrl: './alumnos-form.component.html',
+  styleUrls: ['./alumnos-form.component.css']
+})
+export class AlumnosFormComponent {
+
+  titulo = "Formulario alumnos";
+
+}
