854b49e8fe900adfee515a80fd76fe886f215d5f
Felix Javier Nuñez Gonzalez <33491472+nunez7@users.noreply.github.com>
2023-02-11 14:27:15 -0700

    Agregando servicios

--- src/app/components/cursos/asignar-alumnos.component.ts
+++ src/app/components/cursos/asignar-alumnos.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Curso } from 'src/app/models/curso';
+import { AlumnoService } from 'src/app/services/alumno.service';
+import { CursoService } from 'src/app/services/curso.service';
 
 @Component({
   selector: 'app-asignar-alumnos',
   templateUrl: './asignar-alumnos.component.html',
   styleUrls: ['./asignar-alumnos.component.css']
 })
-export class AsignarAlumnosComponent {
+export class AsignarAlumnosComponent implements OnInit{
 
+  curso: Curso;
+
+  constructor(private route: ActivatedRoute,
+   private cursoService: CursoService,
+   private alumnoService: AlumnoService ){}
+
+   ngOnInit(): void {
+       this.route.paramMap.subscribe(params => {
+        const id: number = + params.get('id');
+        this.cursoService.ver(id).subscribe(c => this.curso= c);
+       });
+   }
 }
