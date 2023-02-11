ed45d90ed4d370eb0fd401198309431779dea4cd
Felix Javier Nuñez Gonzalez <33491472+nunez7@users.noreply.github.com>
2023-02-10 19:47:37 -0700

    Implementando listado y paginado

--- src/app/services/curso.service.ts
+++ src/app/services/curso.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BASE_ENDPOINT } from '../config/app';
 import { Curso } from '../models/curso';
 import { CommonService } from './common.service';
 
@@ -7,10 +8,10 @@ import { CommonService } from './common.service';
   providedIn: 'root'
 })
 export class CursoService extends CommonService<Curso>{
+  protected override baseEndpoint  = BASE_ENDPOINT+'/cursos';
 
   constructor(http: HttpClient) {
     super(http);
   }
-
-  protected override baseEndpoint  = 'http://localhost:8090/api/cursos';
+  
 }
