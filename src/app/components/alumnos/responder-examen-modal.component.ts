a5c5f86f18b2a8f1b357a632aa6a9b20ebb5f4fe
Felix Javier Nuñez Gonzalez <33491472+nunez7@users.noreply.github.com>
2023-02-12 10:39:33 -0700

    Implementando el code del modal
    Video 203

--- src/app/components/alumnos/responder-examen-modal.component.ts
+++ src/app/components/alumnos/responder-examen-modal.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { Alumno } from 'src/app/models/alumno';
+import { Curso } from 'src/app/models/curso';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Examen } from 'src/app/models/examen';
 
 @Component({
   selector: 'app-responder-examen-modal',
   templateUrl: './responder-examen-modal.component.html'
 })
-export class ResponderExamenModalComponent {
+export class ResponderExamenModalComponent implements OnInit{
+
+  curso: Curso;
+  alumno: Alumno;
+  examen: Examen;
+
+  respuestas = ['Alguna'];
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any,
+  public modalRef: MatDialogRef<ResponderExamenModalComponent>){}
+
+  ngOnInit(): void {
+    this.curso = this.data.curso as Curso;
+    this.alumno = this.data.alumno as Alumno;
+    this.examen = this.data.examen as Examen;
+  }
+
+  cancelar(): void{
+    this.modalRef.close();
+  }
 
 }
